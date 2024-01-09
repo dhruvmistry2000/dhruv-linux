61e50b6ca0609674986a2311679875228b734c4c
dhruv mistry <dhruvmistry2000@gmail.com>
2024-01-09 11:09:08 +0530

    update
    scripts adding and install script update

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@ sudo apt install feh preload i3 rofi kitty polybar neofetch bottom
 sudo apt update 
 sudo apt upgrade
 
+#copying all the config files
 cp -r .config/* ~/.config/
 
+# copying my favourite wallpapers
 mkdir ~/wallpaper
 cp -r wallpaper ~/
 
+# copying my useful scripts
+cp -r scripts ~/
