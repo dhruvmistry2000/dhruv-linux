8db60da1af76f72b61ad4ab344aa50aa6d0a2c69
dhruv <dhruvmistry2000@gmail.com>
2024-01-16 10:34:47 +0530

    Removing perosnal

--- .config/Code/User/History/58ee20f7/AqLC.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo apt install feh preload i3 rofi kitty polybar
--- .config/Code/User/History/58ee20f7/GiRw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt install feh preload i3 rofi kitty polybar neofetch bottom
-
-sudo apt update 
-sudo apt upgrade
-
-
-
